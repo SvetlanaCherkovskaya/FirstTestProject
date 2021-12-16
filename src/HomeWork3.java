cdaaceaa03aabe8b48afc078e3872ea920fda4e1
SvetlanaCherkovskaya <79523721004@yandex.ru>
2021-12-16 15:31:24 +0300

    first commit

--- src/HomeWork3.java
+++ src/HomeWork3.java
@@ -2,7 +2,7 @@
  * Java 1. Homework #3
  * <p>
  * author Svetlana Cherkovskaya
- * version 12.12.2021 - 13.12.2021
+ * version 14.12.2021 - 16.12.2021
  */
 
 import java.util.Arrays;
@@ -10,31 +10,27 @@ import java.util.Arrays;
 public class HomeWork3 {
 
     public static void main(String[] args) {
-        makeArray1();
-        makeArray2();
-        makeArray3();
-        makeArray4();
-        System.out.println(Arrays.toString(makeArray5(5, 8)));
-        makeArray6();
-        System.out.println(makeArray7(new int[]{1, 2, 3, 1, 2, 3}));
+        reversArray();
+        makeArray();
+        TransformArray();
+        makeTwoDimensionalArray();
+        System.out.println(Arrays.toString(returnArray(5, 8)));
+        findMaxMin();
+        System.out.println(checkBalance(new int[]{1, 2, 3, 1, 2, 3}));
 
 
     }
 
-    private static void makeArray1() {
+    private static void reversArray() {
         int[] nums = {1, 1, 1, 0, 0, 0};
         System.out.println(Arrays.toString(nums));
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) {
-                nums[i] = 0;
-            } else {
-                nums[i] = 1;
-            }
+            nums[i] =  nums[i] == 1 ? 0: 1;
         }
         System.out.println(Arrays.toString(nums));
     }
 
-    private static void makeArray2() {
+    private static void makeArray() {
         int[] nums = new int[100];
         for (int i = 0; i < nums.length; i++) {
             nums[i] = i + 1;
@@ -42,7 +38,7 @@ public class HomeWork3 {
         System.out.println(Arrays.toString(nums));
     }
 
-    private static void makeArray3() {
+    private static void TransformArray() {
         int[] nums = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         System.out.println(Arrays.toString(nums));
         for (int i = 0; i < nums.length; i++) {
@@ -53,7 +49,7 @@ public class HomeWork3 {
         System.out.println(Arrays.toString(nums));
     }
 
-    private static void makeArray4() {
+    private static void makeTwoDimensionalArray() {
         int[][] nums = new int[5][5];
         for (int i = 0; i < 5; i++) {
             int j;
@@ -66,11 +62,10 @@ public class HomeWork3 {
                 System.out.print(nums[i][j] + " ");
             }
             System.out.println();
-
         }
     }
 
-    private static int[] makeArray5(int len, int initialValue) {
+    private static int[] returnArray(int len, int initialValue) {
         int[] nums = new int[len];
         for (int i = 0; i < nums.length; i++) {
             nums[i] = initialValue;
@@ -78,10 +73,9 @@ public class HomeWork3 {
         {
             return (nums);
         }
-
     }
 
-    private static void makeArray6() {
+    private static void findMaxMin() {
         int[] nums = {1, 2, 8, 4, 9, 6, 3};
         System.out.println(Arrays.toString(nums));
         int max = nums[0];
@@ -100,7 +94,7 @@ public class HomeWork3 {
         System.out.println("Min number = " + min);
     }
 
-    private static boolean makeArray7(int[] arr) {
+    private static boolean checkBalance(int[] arr) {
         int sum = 0;
         int sumLeft = 0;
         int sumRight = 0;
